f36a6d3333285239476e6c1fedfe2c820f2bb47d
beer-psi <lacvtg.a1.2023@gmail.com>
2024-07-30 21:07:14 +0700

    prettier

--- database-seeds/scripts/rerunners/arcaea/merge-switch-local-data.ts
+++ database-seeds/scripts/rerunners/arcaea/merge-switch-local-data.ts
@@ -7,45 +7,45 @@ import { convertDifficulty } from "./_common";
 import { CreateLogger } from "mei-logger";
 
 interface SongInfo {
-    difficulty: 0 | 1 | 2 | 3;
-    song_id: string;
-    rating: number;
+	difficulty: 0 | 1 | 2 | 3;
+	song_id: string;
+	rating: number;
 }
 
 const logger = CreateLogger("arcaea/merge-switch-local-data");
 const program = new Command()
-    .requiredOption("-d, --data <switch-local-xx-yy-zz>")
-    .parse(process.argv);
+	.requiredOption("-d, --data <switch-local-xx-yy-zz>")
+	.parse(process.argv);
 const options = program.opts();
 
-const existingChartDocs: Array<ChartDocument<GPTStrings["arcaea"]>> = ReadCollection("charts-arcaea.json");
+const existingChartDocs: Array<ChartDocument<GPTStrings["arcaea"]>> =
+	ReadCollection("charts-arcaea.json");
 const existingCharts = new Map(
-    existingChartDocs.map((c) => [`${c.data.inGameStrID}-${c.playtype}-${c.difficulty}`, c]),
+	existingChartDocs.map((c) => [`${c.data.inGameStrID}-${c.playtype}-${c.difficulty}`, c])
 );
 const songs = JSON.parse(
-    fs.readFileSync(
-        path.join(options.data, "songs.json"),
-        { encoding: "utf-8" },
-    ),
- ) as SongInfo[];
+	fs.readFileSync(path.join(options.data, "songs.json"), { encoding: "utf-8" })
+) as SongInfo[];
 
 for (const song of songs) {
-    const difficulty = convertDifficulty(song.difficulty);
+	const difficulty = convertDifficulty(song.difficulty);
 
-    // We usually only want to update the chart constants for the controller playtype.
-    // If the Switch version ends up with some more exclusive songs, it is usually trivial to 
-    // update chart constants normally.
-    const chart = existingCharts.get(`${song.song_id}-Controller-${difficulty}`);
+	// We usually only want to update the chart constants for the controller playtype.
+	// If the Switch version ends up with some more exclusive songs, it is usually trivial to
+	// update chart constants normally.
+	const chart = existingCharts.get(`${song.song_id}-Controller-${difficulty}`);
 
-    if (!chart) {
-        logger.warn(`Missing chart ${song.song_id} [Controller ${difficulty}].`);
-        continue;
-    }
+	if (!chart) {
+		logger.warn(`Missing chart ${song.song_id} [Controller ${difficulty}].`);
+		continue;
+	}
 
-    if (chart.levelNum !== song.rating) {
-        logger.info(`Chart ${song.song_id} [Controller ${difficulty}] has had a levelNum change: ${chart.levelNum} -> ${song.rating}`);
-        chart.levelNum = song.rating;
-    }
+	if (chart.levelNum !== song.rating) {
+		logger.info(
+			`Chart ${song.song_id} [Controller ${difficulty}] has had a levelNum change: ${chart.levelNum} -> ${song.rating}`
+		);
+		chart.levelNum = song.rating;
+	}
 }
 
 WriteCollection("charts-arcaea.json", existingChartDocs);
--- database-seeds/scripts/rerunners/arcaea/notecounts/copy-aff.ts
+++ database-seeds/scripts/rerunners/arcaea/notecounts/copy-aff.ts
@@ -3,53 +3,52 @@ import { Command } from "commander";
 import path from "path";
 
 const program = new Command()
-    .requiredOption("-b, --bundle <cb>")
-    .requiredOption("-d, --download <dl>")
-    .option("-o, --output <dir>")
-    .parse(process.argv);
+	.requiredOption("-b, --bundle <cb>")
+	.requiredOption("-d, --download <dl>")
+	.option("-o, --output <dir>")
+	.parse(process.argv);
 const options = program.opts();
 const output = options.output ?? path.join(__dirname, "charts");
 
 if (!existsSync(output)) {
-    mkdirSync(output);
+	mkdirSync(output);
 }
 
 const songsDirectory = path.join(options.bundle, "active", "songs");
 
 for (const id of readdirSync(songsDirectory)) {
-    const dir = path.join(songsDirectory, id);
+	const dir = path.join(songsDirectory, id);
 
-    if (id.startsWith("dl_") || statSync(dir).isFile()) {
-        continue;
-    }
+	if (id.startsWith("dl_") || statSync(dir).isFile()) {
+		continue;
+	}
 
-    const target = path.join(output, id);
+	const target = path.join(output, id);
 
-    if (!existsSync(target)) {
-        mkdirSync(target);
-    }
-    
+	if (!existsSync(target)) {
+		mkdirSync(target);
+	}
 
-    for (const file of readdirSync(dir)) {
-        if (!file.endsWith(".aff")) {
-            continue;
-        }
+	for (const file of readdirSync(dir)) {
+		if (!file.endsWith(".aff")) {
+			continue;
+		}
 
-        copyFileSync(path.join(songsDirectory, id, file), path.join(target, file));
-    }
+		copyFileSync(path.join(songsDirectory, id, file), path.join(target, file));
+	}
 }
 
 for (const file of readdirSync(options.download)) {
-    if (!/^[a-z]+_\d/.test(file)) {
-        continue;
-    }
+	if (!/^[a-z]+_\d/u.test(file)) {
+		continue;
+	}
 
-    const [id, difficulty] = file.split("_") as [string, string];
-    const target = path.join(output, id);
+	const [id, difficulty] = file.split("_") as [string, string];
+	const target = path.join(output, id);
 
-    if (!existsSync(target)) {
-        mkdirSync(target);
-    }
-    
-    copyFileSync(path.join(options.download, file), path.join(target, `${difficulty}.aff`));
+	if (!existsSync(target)) {
+		mkdirSync(target);
+	}
+
+	copyFileSync(path.join(options.download, file), path.join(target, `${difficulty}.aff`));
 }
